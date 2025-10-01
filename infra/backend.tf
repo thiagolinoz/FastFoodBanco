7e962a45d2c03290a653f3dc3a7f3afbb03e51bb
thiagolinoz <thiagolinoz@gmail.com>
2025-10-01 09:52:03 -0300

    cria s3 no deply action

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-fiap-fastfood-backend-banco-lino"
+    bucket = "postech-fiap-fastfood-backend-banco-lino" #TODO trocar nome do bucket
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
@@ -9,7 +9,7 @@ terraform {
 data "terraform_remote_state" "eks_vpc" {
   backend = "s3"
   config = {
-    bucket = "postech-fiap-fastfood-backend-eks-lino"
+    bucket = "postech-fiap-fastfood-backend-eks-lino" #TODO trocar nome do bucket
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
