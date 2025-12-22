ca4651726a06988bf8d322fba1732dac3172fcb8
thiagolinoz <thiagolinoz@gmail.com>
2025-12-22 20:11:32 -0300

    ajuste p fase 4

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-fiap-fastfood-backend-banco-lino" #TODO trocar nome do bucket
+    bucket = "postech-fiap-fastfood-backend-banco-fase4" #TODO trocar nome do bucket
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
@@ -9,7 +9,7 @@ terraform {
 data "terraform_remote_state" "eks_vpc" {
   backend = "s3"
   config = {
-    bucket = "postech-fiap-fastfood-backend-eks-lino" #TODO trocar nome do bucket
+    bucket = "postech-fiap-fastfood-backend-eks-fase4-dynamo" #TODO trocar nome do bucket
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
