93aadd2d69aa60d1282082f95544063c52995e43
thiagolinoz <thiagolinoz@gmail.com>
2025-12-22 20:13:17 -0300

    ajusta bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-fiap-fastfood-backend-banco-fase4" #TODO trocar nome do bucket
+    bucket = "postech-fiap-fastfood-backend-banco-fase4-final" #TODO trocar nome do bucket
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
